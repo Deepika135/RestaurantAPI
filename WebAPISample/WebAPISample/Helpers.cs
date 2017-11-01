65bf5d27aa8bf16ea58a6d5982ce5b3c75fca278
Deepika135 <deepika.reddys135@gmail.com>
2017-10-31 17:35:52 -0700

    Added API for restaurant reviews

---
+++ WebAPISample/WebAPISample/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPISample
+{
+	public class Helpers
+	{
+		public const string SampleApiRoute_Restaurant_List = "api/v1/RestaurantList";
+
+		public const string SampleApiRoute_Restaurant_Add = "api/v1/AddRestaurant";
+
+		public const string SampleApiRoute_Restaurant_AddReview = "api/v1/Restaurant/AddReview";
+
+		public const string SampleApiRoute_Restaurant_Reviews_ListByUser = "api/v1/Restaurant/UserReviews";
+
+		public const string SampleApiRoute_Restaurant_DeleteReview = "api/v1/Restaurant/DeleteReview";
+	}
+}
